014b31916a3033dc1435bd81c6d750f21f456267
Labib Khan <67333632+labibkhanofficialbd@users.noreply.github.com>
2021-06-29 22:35:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function changeHero(color){
+    const span = document.querySelector('.hero');
+    span.style.color= color;
+}
+
+function changeButn(color){
+    const a = document.querySelector('.butn');
+    a.style.background= color;
+}
